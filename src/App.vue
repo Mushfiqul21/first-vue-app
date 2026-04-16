2efaeb228e219a5341d46531f94957484d9c1aed
Mushfiqul21 <mushfiqulislam21@gmail.com>
2026-04-17 00:06:26 +0600

    starting todo app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script setup>
 import { ref } from 'vue'
-const counter = ref('0')
-const dec = () => {
-  if (counter.value > 0) {
-    counter.value--
-  }
-}
+const newtask = ref('')
 </script>
 
 <template>
-  <h1>Counter is: {{ counter }}</h1>
-  <button @click = "counter++">increment +1</button>
-  <button @click = "dec">decrement -1</button>
-
+  <div class="wrapper">
+    <h1>Todo App</h1>
+    <div class="input-row">
+      <input type="text" placeholder="Add Task here..." v-model="newtask">
+      <button>Add</button>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app{
+  max-width: 500px;
+  margin: 2rem;
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+}
+</style>

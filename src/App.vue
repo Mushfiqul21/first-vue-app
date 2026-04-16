d91b33257048d11bb1690c6529206c00f6c458c4
Mushfiqul21 <mushfiqulislam21@gmail.com>
2026-04-16 23:27:46 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const counter = ref('0')
+const dec = () => {
+  if (counter.value > 0) {
+    counter.value--
+  }
+}
+</script>
+
+<template>
+  <h1>Counter is: {{ counter }}</h1>
+  <button @click = "counter++">increment +1</button>
+  <button @click = "dec">decrement -1</button>
+
+</template>
+
+<style scoped></style>
